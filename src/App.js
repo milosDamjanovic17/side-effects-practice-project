03c9cf828eb678c2e35266c44912371e890e0b0c
Milos Damjanovic <milos.10001@yahoo.com>
2023-04-13 18:29:10 -0400

    added useEffect side effect without dependencies

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -10,10 +10,20 @@ function App() {
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
+  useEffect(() => {
+    const storedUserLoginInfo = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoginInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
