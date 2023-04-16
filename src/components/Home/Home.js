b29717f7f5f7610aeab5c01aa71f0428097b63a9
Milos Damjanovic <milos.10001@yahoo.com>
2023-04-16 20:29:32 +0200

    notes updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,7 @@ const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={authCtx.onLogout}>Logout</Button> {/* we'll still use props because we want to make sure that this button ONLY does logout function */}
+      <Button onClick={authCtx.onLogout}>Logout</Button> {/* DEPRECATED, ITS UPDATED TO authCtx context we'll still use props because we want to make sure that this button ONLY does logout function */}
     </Card>
   );
 };
